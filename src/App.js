b19f6afbd5aacbc3253d173dbc7ec65d0a5b2400
Thomas Nelson <thnlsn@gmail.com>
2021-03-02 12:23:00 -0800

    created starter css variables as well as constructed a dev environment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './css/style.css';
 
 function App() {
-  return <div className='App'>▓▓▓▓▓Test▓▓▓▓▓</div>;
+  return <div className='App'>Thomas Nelson</div>;
 }
 
 export default App;
