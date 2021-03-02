1a6a2a2ac55404ed0ea9b21feff5528600e6e72e
Thomas Nelson <thnlsn@gmail.com>
2021-03-02 14:19:06 -0800

    basic header markup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import './css/style.css';
 
+import profile from './images/profile.jpg';
+
 function App() {
-  return <div className='App'>Thomas Nelson</div>;
+  return (
+    <div className='App'>
+      <div className='navigation'></div>
+      <div className='content'>
+        <header className='header'>
+          <img
+            className='heading-image'
+            src={profile}
+            alt='Thomas Nelson'
+          ></img>
+          <div className='heading-primary'>Thomas Nelson</div>
+          <div className='heading-subtitle'>web developer & problem-solver</div>
+        </header>
+      </div>
+    </div>
+  );
 }
 
 export default App;
