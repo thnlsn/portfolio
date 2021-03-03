d54763c30592e3d9ba02b1aabdf3b90c6abae684
Thomas Nelson <thnlsn@gmail.com>
2021-03-02 21:46:10 -0800

    second section markup and reusable styles applied

--- src/App.js
+++ src/App.js
@@ -32,10 +32,7 @@ function App() {
         <Header img={profile} />
         <section className='section section--who'>
           <div className='heading-secondary'>
-            <span
-              className='section--emphasis section--subject'
-              data-subject={'#who'}
-            >
+            <span className='section--subject' data-subject={'#who'}>
               #who
             </span>{' '}
             I am
@@ -44,17 +41,32 @@ function App() {
             A web developer on and off the clock and a student beyond the
             classroom. I love spending my time coding and building websites that
             are robust, functional, and beautiful. Check out my{' '}
-            <a href={'#portfolio'} className='section--emphasis'>
+            <a href={'#portfolio'} className='section--link'>
               work
             </a>{' '}
             to see what I mean!
           </p>
           <p className='section__description'>
             I’m currently available for projects no matter the scale.{' '}
-            <a href={'#contact'} className='section--emphasis'>
+            <a href={'#contact'} className='section--link'>
               Contact
             </a>{' '}
             me to discuss.
+          </p>
+        </section>
+
+        <section className='section section--portfolio'>
+          <div className='heading-secondary'>
+            <span className='section--subject' data-subject={'#portfolio'}>
+              #portfolio
+            </span>{' '}
+            of work
+          </div>
+          <p className='section__description'>
+            Learning is a lifestyle for me so I strive to learn something new
+            every single day, and I never shy away from a challenge. At this
+            moment I am proficient in the following technologies, but I am
+            always adding to the list.
           </p>
         </section>
       </div>
