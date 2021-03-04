7bb4fd6804ec271bbc676dc038b2cdd5f0960680
Thomas Nelson <thnlsn@gmail.com>
2021-03-03 22:09:45 -0800

    created component for projects and passed in neccesary info as props

---
+++ src/components/Project.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Project = ({ title, img, skills, paragraphs, links }) => {
+  return (
+    <div className='project'>
+      <div className='project__heading'>{title}</div>
+      <img className='project__img' src={img} alt={title} />
+      <div className='project__skills'>
+        {skills.map((skill) => (
+          <div className='project__skill'>{skill}</div>
+        ))}
+      </div>
+      <div className='project__description'>
+        {paragraphs.map((paragraph) => (
+          <p className='project__description'>{paragraph}</p>
+        ))}
+      </div>
+      <div className='links'>
+        {links.map((link) => (
+          <a href={link} className='project__link'></a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Project;
