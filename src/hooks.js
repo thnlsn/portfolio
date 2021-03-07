cf1539134fc35b8ff1b00effb4ad4a44bdd71c56
Thomas Nelson <thnlsn@gmail.com>
2021-03-07 14:13:56 -0800

    added an argument to hook determine whether intersection should activate every time it is intersected or only the first time

--- src/hooks.js
+++ src/hooks.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 
 // Hook to handle intersection observing taking in IntersectionObserver options
-const useOnScreen = function (options) {
+const useOnScreen = function (options, once = true) {
   // ref will be a direct reference to the dom element it is attached to
   const [ref, setRef] = useState(null); // Will reference a DOM element
   const [visible, setVisible] = useState(false); // visible should be false by default
@@ -10,20 +10,23 @@ const useOnScreen = function (options) {
   useEffect(() => {
     const observer = new IntersectionObserver(([entry]) => {
       // This is what we call if intersecting
-      if (entry.isIntersecting) setVisible(true); // If the DOM element is intersecting based on options, set visible to true forever (so it won't activate again)
+      if (entry.isIntersecting) setVisible(entry.isIntersecting);
+      // If false is passed in for once, then it should switch visible to false when leaving intersection, so it can be animated again when intersected
+      else once || setVisible(entry.isIntersecting);
     }, options);
 
+    console.log('ob');
     // Refs sometimes wont be referencing a DOM element on screen, which means ref could be null, so only proceed if it isn't falsy
     if (ref) observer.observe(ref); // If it is though, attach observer to it
 
     // useEffect can optionally return a function that acts as a clean-up function if either of the variables it is watching for changes
     // Unobserve it if it is referencing a DOM element, so as clean-up it will unobserve
     return () => {
+      console.log('unob');
       if (ref) observer.unobserve(ref);
     };
+    // Watch ref and visible, so
   }, [ref, options]);
-
-  console.log(ref, visible);
 
   // Return the setRef function and the value of visible
   // Return setRef instead of ref because of cases where we want IntersectionObserver to conditionally render an entire element, where a ref would not exist at first.
