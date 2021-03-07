8dbf9e809570863b843427b053fc6551827a70e4
Thomas Nelson <thnlsn@gmail.com>
2021-03-07 15:54:21 -0800

    applied observers to each section and created animations to activate when intersected, all working properly

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 import Heading from './SectionHeading';
 
+import { useOnScreen } from '../hooks';
+
 const Section = ({ heading, contents }) => {
+  const [setRef, visible] = useOnScreen({
+    rootMargin: '-200px',
+  });
+
+  const visibility = () => (visible ? 'visible' : 'invisible');
+
   return (
-    <section className='section'>
+    <section className={`section section--${visibility()}`} ref={setRef}>
       <Heading heading={heading} />
       {contents}
     </section>
